f2fd91c24437bbd56189c81fd62cceec938f843b
Jungheon Lee <cutehammond772@gmail.com>
2023-07-20 22:34:07 +0900

    [Platinum IV] Title: 북서풍, Time: 3916 ms, Memory: 217384 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.py
+++ 백준/Platinum/5419. 북서풍/북서풍.py
@@ -1,11 +1,12 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+# 좌표 압축
 def compress(N, C):
-  ordered = sorted(range(N), key=lambda x: C[x][1])
+  indexes = sorted(range(N), key=lambda x: C[x][1])
   order, previous = 0, 10 ** 9 + 1
 
-  for idx in ordered:
+  for idx in indexes:
     if previous != C[idx][1]:
       order += 1
 
@@ -17,17 +18,18 @@ def compress(N, C):
 def query(tree, q):
   result = 0
 
-  while q > 0:
+  while q:
     result += tree[q]
-    q -= (q & -q)
+    q -= q & -q
 
   return result
 
 def update(tree, q, x):
   while 0 < q < len(tree):
     tree[q] += x
-    q += (q & -q)
+    q += q & -q
 
+# 펜윅 트리 이용
 def solve(N, C):
   result = 0
   tree = [0] * ((L := compress(N, C)) + 1)
@@ -44,7 +46,7 @@ if __name__ == '__main__':
     N = int(input())
 
     # x값 오름차순, y값 내림차순으로 좌표 정렬
-    coordinates = [[*map(int, input().split())] for _ in range(N)]
-    coordinates.sort(key=lambda k: (k[0], -k[1]))
+    C = [[*map(int, input().split())] for _ in range(N)]
+    C.sort(key=lambda k: (k[0], -k[1]))
 
-    print(solve(N, coordinates))
+    print(solve(N, C))
