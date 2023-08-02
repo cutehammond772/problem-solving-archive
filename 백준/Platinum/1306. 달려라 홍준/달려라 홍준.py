a9df3eaf29eb83c48a7b5522c9e66bbaf9d4cbda
Jungheon Lee <cutehammond772@gmail.com>
2023-08-02 10:17:19 +0900

    [Platinum V] Title: 달려라 홍준, Time: 5508 ms, Memory: 262756 KB -BaekjoonHub

--- 백준/Platinum/1306. 달려라 홍준/달려라 홍준.py
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.py
@@ -1,42 +1,43 @@
 import sys, math
 input = lambda: sys.stdin.readline().rstrip()
 
+def make(tree, A, node, left, right):
+  if left == right:
+    tree[node] = A[left]
+    return
+
+  mid = (left + right) // 2
+  make(tree, A, node * 2, left, mid)
+  make(tree, A, node * 2 + 1, mid + 1, right)
+
+  tree[node] = max(tree[node * 2], tree[node * 2 + 1])
+
 def init(N, A):
   tree = [0] * (2 ** (math.ceil(math.log2(N)) + 1))
+  make(tree, A, 1, 0, N - 1)
 
-  def make(node, left, right):
-    if left == right:
-      tree[node] = A[left]
-      return
-
-    mid = (left + right) // 2
-    make(node * 2, left, mid)
-    make(node * 2 + 1, mid + 1, right)
-
-    tree[node] = max(tree[node * 2], tree[node * 2 + 1])
-
-  make(1, 0, N - 1)
   return tree
 
-def query(N, tree, x, y):
-  def subquery(node, left, right):
-    if x <= left and right <= y:
-      return tree[node]
+def query(tree, x, y, node, left, right):
+  if x <= left and right <= y:
+    return tree[node]
 
-    if y < left or right < x:
-      return 0
+  if y < left or right < x:
+    return 0
 
-    mid = (left + right) // 2
-    return max(subquery(node * 2, left, mid), subquery(node * 2 + 1, mid + 1, right))
+  mid = (left + right) // 2
 
-  return subquery(1, 0, N - 1)
+  return max(
+    query(tree, x, y, node * 2, left, mid),
+    query(tree, x, y, node * 2 + 1, mid + 1, right)
+  )
 
 def solve(N, M, A):
   result = []
   tree = init(N, A)
 
   for x in range(M - 1, N - (M - 1)):
-    result.append(query(N, tree, x - (M - 1), x + (M - 1)))
+    result.append(query(tree, x - (M - 1), x + (M - 1), 1, 0, N - 1))
 
   return result
 
