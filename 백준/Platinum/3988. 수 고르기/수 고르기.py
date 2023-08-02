62ba99dd9dd91c2b5fd22a5bab3d7fc3c4246058
Jungheon Lee <cutehammond772@gmail.com>
2023-08-02 09:53:08 +0900

    [Platinum IV] Title: 수 고르기, Time: 760 ms, Memory: 251704 KB -BaekjoonHub

---
+++ 백준/Platinum/3988. 수 고르기/수 고르기.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+INF = 20000001
+
+# 1. Monotone Queue 테크닉을 이용한다. (= 최솟값)
+# 2. 최대값은 무조건 [x, x + (N - K) - 1]의 양끝의 차이다.
+if __name__ == '__main__':
+  N, K = map(int, input().split())
+  V = [*sorted(map(int, input().split()))]
+
+  result = INF
+
+  idx = 1
+  queue = deque([])
+
+  # 슬라이딩 윈도우
+  for offset in range(K + 1):
+    # 범위에 해당되지 않는 원소 제거
+    if queue and queue[0] <= offset:
+      queue.popleft()
+
+    for x in range(idx, offset + (N - K)):
+      while queue and (V[x] - V[x - 1]) <= (V[queue[-1]] - V[queue[-1] - 1]):
+        queue.pop()
+
+      queue.append(x)
+
+    idx = offset + (N - K)
+    M, m = V[idx - 1] - V[offset], V[queue[0]] - V[queue[0] - 1]
+
+    result = min(result, M + m)
+
+  print(result)
