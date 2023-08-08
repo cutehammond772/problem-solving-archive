1abbbb184cfa5eb626d414242688a880eca795d8
Jungheon Lee <cutehammond772@gmail.com>
2023-08-08 21:03:06 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 248 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.py
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.py
@@ -1,24 +1,22 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 MOD = 10007
-off = ord('a')
 
 if __name__ == '__main__':
   S = input()
   L = len(S)
 
   # 인덱스 i부터 존재하는 팰린드롬 개수
-  memo = [0] * len(S)
+  memo = [0] * L
 
   for x in range(L):
-    char = ord(S[x]) - off
-    accumulation = 1
-    memo[x] += accumulation
+    accu = 1
+    memo[x] += accu
 
     for y in range(x - 1, -1, -1):
-      if ord(S[y]) - off == char:
-        accumulation = (memo[y + 1] + 1) % MOD
+      if S[x] == S[y]:
+        accu = (memo[y + 1] + 1) % MOD
 
-      memo[y] = (memo[y] + accumulation) % MOD
+      memo[y] = (memo[y] + accu) % MOD
 
   print(memo[0])
