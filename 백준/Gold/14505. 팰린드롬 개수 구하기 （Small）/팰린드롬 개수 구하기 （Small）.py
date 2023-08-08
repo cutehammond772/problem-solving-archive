10fecf69516ea2d015476585921cae49b78a5001
Jungheon Lee <cutehammond772@gmail.com>
2023-08-08 20:11:42 +0900

    [Gold III] Title: 팰린드롬 개수 구하기 (Small), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/14505. 팰린드롬 개수 구하기 （Small）/팰린드롬 개수 구하기 （Small）.py
@@ -0,0 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+off = ord('a')
+
+if __name__ == '__main__':
+  S = input()
+  L = len(S)
+
+  # 인덱스 i부터 존재하는 팰린드롬 개수
+  memo = [0] * len(S)
+
+  for x in range(L):
+    char = ord(S[x]) - off
+    accumulation = 1
+    memo[x] += accumulation
+
+    for y in range(x - 1, -1, -1):
+      if ord(S[y]) - off == char:
+        accumulation = memo[y + 1] + 1
+
+      memo[y] = memo[y] + accumulation
+
+  print(memo[0])
