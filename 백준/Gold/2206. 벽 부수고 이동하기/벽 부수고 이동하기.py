eb3b10ecf7a04f53d027792e3f9204647272d9d5
Jungheon Lee <cutehammond772@gmail.com>
2023-09-06 13:33:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 908 ms, Memory: 236572 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,66 +1,59 @@
 import sys
-from queue import Queue
-input = lambda: sys.stdin.readline().strip()
-
-MAX_DIST = 1000000
-PATH, WALL = 0, 1
-
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+INF = 10 ** 6 + 1
 dr, dc = [0, 1, 0, -1], [1, 0, -1, 0]
 
-def solve(maze, N, M):
-  # 벽을 부수지 않았을 경우
-  dist = [[MAX_DIST] * M for _ in range(N)]
-  # 벽을 부쉈을 경우
-  b_dist = [[MAX_DIST] * M for _ in range(N)]
-  
-  que = Queue()
-  
-  # (row, col, 벽을 부순 여부)
-  que.put((0, 0, False))
-  dist[0][0] = 1
-  
-  while not que.empty():
-    row, col, broken = que.get()
-    
-    # 벽을 부수지 않았을 경우
-    if not broken:
-      for i in range(4):
-        nrow, ncol = row + dr[i], col + dc[i]
-        
-        if not ((0 <= nrow < N) and (0 <= ncol < M)):
-          continue
-          
-        if dist[nrow][ncol] > dist[row][col] + 1:
-          dist[nrow][ncol] = dist[row][col] + 1
-          
-          if maze[nrow][ncol] == WALL:
-            que.put((nrow, ncol, True))
-            b_dist[nrow][ncol] = dist[nrow][ncol]
-          else:
-            que.put((nrow, ncol, False))
-            
-    # 벽을 부쉈을 경우      
-    else:
-      for i in range(4):
-        nrow, ncol = row + dr[i], col + dc[i]
-        
-        if not ((0 <= nrow < N) and (0 <= ncol < M)):
-          continue
-          
-        if maze[nrow][ncol] == WALL:
-          continue
-          
-        if b_dist[nrow][ncol] > b_dist[row][col] + 1:
-          b_dist[nrow][ncol] = b_dist[row][col] + 1
-          que.put((nrow, ncol, True))
+def traverse(N, M, A, wall, point):
+	queue = deque([(*point, 1)])
+	A[point[0]][point[1]] = 1
 
-  result = min(dist[N - 1][M - 1], b_dist[N - 1][M - 1])
-  return result if result != MAX_DIST else -1
-      
-          
-if __name__ == '__main__':
-  N, M = map(int, input().split())
-  maze = [[int(chr) for chr in input()] for _ in range(N)]
-  
-  result = solve(maze, N, M)
-  print(result)
+	while queue:
+		row, col, dist = queue.popleft()
+
+		for x in range(4):
+			nrow, ncol = row + dr[x], col + dc[x]
+
+			if not (0 <= nrow < N and 0 <= ncol < M):
+				continue
+
+			if A[nrow][ncol] <= dist + 1:
+				continue
+
+			if wall[nrow][ncol]:
+				continue
+
+			A[nrow][ncol] = dist + 1
+			queue.append((nrow, ncol, dist + 1))
+
+def solve(N, M, wall):
+	S = [[INF] * M for _ in range(N)]
+	E = [[INF] * M for _ in range(N)]
+
+	traverse(N, M, S, wall, (0, 0))
+	traverse(N, M, E, wall, (N - 1, M - 1))
+
+	result = min(S[N - 1][M - 1], E[0][0])
+
+	for row in range(N):
+		for col in range(M):
+			if not wall[row][col]:
+				continue
+
+			for a in range(4 - 1):
+				for b in range(a + 1, 4):
+					arow, acol = row + dr[a], col + dc[a]
+					brow, bcol = row + dr[b], col + dc[b]
+
+					if not (0 <= arow < N and 0 <= acol < M) or not (0 <= brow < N and 0 <= bcol < M):
+						continue
+
+					result = min(result, S[arow][acol] + E[brow][bcol] + 1, S[brow][bcol] + E[arow][acol] + 1)
+
+	return -1 if result == INF else result
+
+if __name__ == "__main__":
+	N, M = map(int, input().split())
+	matrix = [[int(c) for c in input()] for _ in range(N)]
+
+	print(solve(N, M, matrix))
