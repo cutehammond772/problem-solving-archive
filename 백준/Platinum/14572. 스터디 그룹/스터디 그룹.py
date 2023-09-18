f8d101686dcc0ddf3a8e8d11a86cb57a7b940e8b
Jungheon Lee <cutehammond772@gmail.com>
2023-09-18 13:24:52 +0900

    [Platinum V] Title: 스터디 그룹, Time: 872 ms, Memory: 167060 KB -BaekjoonHub

---
+++ 백준/Platinum/14572. 스터디 그룹/스터디 그룹.py
@@ -0,0 +1,56 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve(N, K, D, students, algorithms):
+	# (실력) 오름차순으로 정렬한다.
+	students.sort()
+	x = 0
+
+	# 해당 알고리즘을 알고 있는 학생의 수이다.
+	check = [0] * (K + 1)
+	algo_count, stud_count, result = 0, 0, 0
+
+	# 모든 학생들이 알아야 효율성이 줄어든다. 즉 학생을 최대한 많이 잡는 것이 이득이다.
+	for y in range(N):
+		dx, ix = students[x]
+		dy, iy = students[y]
+
+		# 1. y번째 학생을 그룹에 포함시킨다.
+		for alg in algorithms[iy]:
+			if not check[alg]:
+				algo_count += 1
+
+			check[alg] += 1
+
+		stud_count += 1
+
+		# 2. 항상 실력차가 D 이하가 되도록 유지한다.
+		while dy - dx > D:
+			for alg in algorithms[ix]:
+				check[alg] -= 1
+
+				if not check[alg]:
+					algo_count -= 1
+
+			stud_count -= 1
+			x += 1
+
+			dx, ix = students[x]
+
+		result = max(result, stud_count * (algo_count - check.count(stud_count)))
+
+	return result
+
+if __name__ == "__main__":
+	N, K, D = map(int, input().split())
+	students = []
+	algorithms = []
+
+	for x in range(N):
+		M, d = map(int, input().split())
+		A = [*map(int, input().split())]
+
+		students.append((d, x))
+		algorithms.append(A)
+
+	print(solve(N, K, D, students, algorithms))
