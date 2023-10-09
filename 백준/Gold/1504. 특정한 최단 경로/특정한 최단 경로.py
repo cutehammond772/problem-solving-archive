cbc434cde5fd2bd95b3cb49fea75b12b289c80b5
Jungheon Lee <cutehammond772@gmail.com>
2023-10-09 16:55:19 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 396 ms, Memory: 130992 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,70 +1,52 @@
 import sys
 from heapq import heappush, heappop
-
 input = lambda: sys.stdin.readline().rstrip()
-NaN = 799 * 1000 + 1
 
-def dijkstra(N, edges, P, Q):
-  if P == Q:
-    return 0
-    
-  heap = []
-  dist = [NaN] * N
+INF = 8000001
 
-  dist[P] = 0
-  heappush(heap, (0, P))
+def dijkstra(N, adj, A, B):
+	dist = [INF] * (N + 1)
+	queue = []
 
-  while heap:
-    _, idx = heappop(heap)
+	# 시작 정점 추가
+	dist[A] = 0
+	heappush(queue, (0, A))
 
-    for edge in edges[idx]:
-      cost, next = edge
+	while queue:
+		cur_dist, node = heappop(queue)
 
-      if dist[next] > dist[idx] + cost:
-        dist[next] = dist[idx] + cost
+		for next, cost in adj[node]:
+			if cur_dist + cost >= dist[next]:
+				continue
 
-        if next != Q:
-          heappush(heap, (cost, next))
+			dist[next] = cur_dist + cost
 
-  return dist[Q]
+			# 도착 정점일 시 추가 X
+			if next != B:
+				heappush(queue, (cur_dist + cost, next))
 
+	return dist[B]
 
-if __name__ == '__main__':
-  N, E = map(int, input().split())
-  edges = [set() for _ in range(N)]
+def solve(N, adj, V1, V2):
+	start_v1 = dijkstra(N, adj, 1, V1)
+	start_v2 = dijkstra(N, adj, 1, V2)
 
-  for _ in range(E):
-    P, Q, C = map(int, input().split())
-    edges[P - 1].add((C, Q - 1))
-    edges[Q - 1].add((C, P - 1))
+	v1_v2 = dijkstra(N, adj, V1, V2)
 
-  V1, V2 = map(int, input().split())
+	v1_end = dijkstra(N, adj, V1, N)
+	v2_end = dijkstra(N, adj, V2, N)
 
-  # V1부터 V2까지의 최단거리
-  v1_v2 = dijkstra(N, edges, V1 - 1, V2 - 1)
-  
-  # 1부터 V1까지의 최단거리
-  start_to_v1 = dijkstra(N, edges, 0, V1 - 1)
+	result = min(INF, start_v1 + v1_v2 + v2_end, start_v2 + v1_v2 + v1_end)
+	return result if result < INF else -1
 
-  # 1부터 V2까지의 최단거리
-  start_to_v2 = dijkstra(N, edges, 0, V2 - 1)
-  
-  # V1부터 N까지의 최단거리
-  v1_to_dest = dijkstra(N, edges, V1 - 1, N - 1)
+if __name__ == "__main__":
+	N, E = map(int, input().split())
+	adj = [[] for _ in range(N + 1)]
 
-  # V2부터 N까지의 최단거리
-  v2_to_dest = dijkstra(N, edges, V2 - 1, N - 1)
+	for _ in range(E):
+		a, b, c = map(int, input().split())
+		adj[a].append((b, c))
+		adj[b].append((a, c))
 
-  result = NaN * 3
-  
-  # 케이스 1: [1 -> V1 -> V2 -> N]
-  result_v1_v2 = [start_to_v1, v1_v2, v2_to_dest]
-  if NaN not in result_v1_v2:
-    result = min(result, sum(result_v1_v2))
-    
-  # 케이스 2: [1 -> V2 -> V1 -> N]
-  result_v2_v1 = [start_to_v2, v1_v2, v1_to_dest]
-  if NaN not in result_v2_v1:
-    result = min(result, sum(result_v2_v1))  
-
-  print(result if result != 3 * NaN else -1)
+	V1, V2 = map(int, input().split())
+	print(solve(N, adj, V1, V2))
