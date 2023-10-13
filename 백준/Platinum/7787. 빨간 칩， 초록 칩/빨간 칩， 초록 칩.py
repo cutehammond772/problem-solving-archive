5905992debe799c2b5f2b17626dc528ecc66e4eb
Jungheon Lee <cutehammond772@gmail.com>
2023-10-13 21:38:48 +0900

    [Platinum IV] Title: 빨간 칩, 초록 칩, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/7787. 빨간 칩， 초록 칩/빨간 칩， 초록 칩.py
+++ 백준/Platinum/7787. 빨간 칩， 초록 칩/빨간 칩， 초록 칩.py
@@ -1,12 +1,16 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+# G(x)를 하나의 블록으로 생각하면,
+# 1 * 1, 2 * 2, 4 * 4, 8 * 8, ..., (2 ^ n) * (2 ^ n) 크기의
+# 블록으로 묶을 때 특유의 규칙이 존재한다.
 def solve(A, B):
   ra, rb = A % 4, B % 4
 
   if ra == rb == 0:
     return solve(A >> 2, B >> 2)
 
+  # 4n + 1, 2, 3번째 블록에서 1번째 블록과 3번째 블록의 대표값은 같다.
   if (ra, rb) in [(1, 1), (2, 2), (3, 3), (1, 3), (3, 1)]:
     return 'B'
 
