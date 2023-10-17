63d08ba3ecc452fef64c8b1958e1dc0732531177
Jungheon Lee <cutehammond772@gmail.com>
2023-10-17 18:42:17 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 8508 ms, Memory: 937240 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,59 +1,29 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-def calculate(N, X, Y):
-  offset = 0
-  result = [0] * (N * N)
-  
-  for p in range(N):
-    for q in range(N):
-      result[offset] = X[p] + Y[q]
-      offset += 1
+def resolve(X, Y):
+  result = dict()
 
-  result.sort()
+  for i in X:
+    for j in Y:
+      if i + j in result: result[i + j] += 1
+      else: result[i + j] = 1
+
   return result
 
-def lb(A, K):
-  x, y = 0, len(A)
-  
-  while x < y:
-    mid = (x + y) // 2
-    if A[mid] >= K:
-      y = mid
-    else:
-      x = mid + 1
-  
-  return x
-
-def ub(A, K):
-  x, y = 0, len(A)
-  
-  while x < y:
-    mid = (x + y) // 2
-    if A[mid] > K:
-      y = mid
-    else:
-      x = mid + 1
-  
-  return x - 1
-
-def solve(N, A, B, C, D):
-  result = 0
-
-  P = calculate(N, A, B)
-  Q = calculate(N, C, D)
-  
-  for idx in range(N * N):
-    k = -P[idx]
-    result += ub(Q, k) - lb(Q, k) + 1
-  
-  return result
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   N = int(input())
   A, B, C, D = [0] * N, [0] * N, [0] * N, [0] * N
-  
+
   for x in range(N):
     A[x], B[x], C[x], D[x] = map(int, input().split())
 
-  print(solve(N, A, B, C, D))
+  Q = resolve(C, D)
+  count = 0
+
+  for x in A:
+    for y in B:
+      if -(x + y) in Q:
+        count += Q[-(x + y)]
+
+  print(count)
