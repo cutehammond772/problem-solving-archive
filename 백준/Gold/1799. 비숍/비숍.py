1b9ffcb9d6741ed02f20e07d986be66feff7c56b
Jungheon Lee <cutehammond772@gmail.com>
2023-10-17 21:05:20 +0900

    [Gold I] Title: 비숍, Time: 832 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -1,53 +1,29 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-def solve(N, blocks, offset):
+def solve(N, matrix, off):
   result = 0
-  bishops = [[False] * N for _ in range(N)]
 
-  def first(row):
-    return offset if row % 2 == 0 else (1 - offset)
-
-  def invalid(row, col):
-    if blocks[row][col]:
-      return True
-
-    for x in range(row):
-      lcol, rcol = col - (row - x), col + (row - x)
-      
-      if lcol >= 0 and bishops[x][lcol]:
-        return True
-        
-      if rcol < N and bishops[x][rcol]:
-        return True
-        
-    return False
-  
-  def recursion(row_off, col_off, count):
+  def check(row, col, l, r, count):
     nonlocal result
-    
-    for row in range(row_off, N):
-      for col in range(first(row), N, 2):
-        if row == row_off and col < col_off:
-          continue
-          
-        if invalid(row, col):
-          continue
 
-        bishops[row][col] = True
-        recursion(row, col + 2, count + 1)
-        bishops[row][col] = False
-        
-    result = max(result, count)
+    if row == N:
+      result = max(result, count)
+      return
 
-  recursion(0, offset, 0)
+    for next in range(col, N, 2):
+      if matrix[row][next] == 0 or ((l | r) & 1 << next):
+        continue
+
+      check(row, next + 2, l | 1 << next, r | 1 << next, count + 1)
+
+    check(row + 1, (off + (row + 1)) % 2, l >> 1, r << 1, count)
+
+  check(0, off, 0, 0, 0)
   return result
 
-def convert(x):
-  return int(x) == 0
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   N = int(input())
-  blocks = [[*map(convert, input().split())] for _ in range(N)]
+  matrix = [[*map(int, input().split())] for _ in range(N)]
 
-  print(solve(N, blocks, 0) + solve(N, blocks, 1))
+  print(solve(N, matrix, 0) + solve(N, matrix, 1))
