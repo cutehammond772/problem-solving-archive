359bbf4aad4047cbf3286fa1c37b099b329cd7c3
Jungheon Lee <cutehammond772@gmail.com>
2023-10-18 14:26:55 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 476 ms, Memory: 68200 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -2,35 +2,20 @@ import sys
 from math import floor, sqrt, ceil
 input = lambda: sys.stdin.readline().rstrip()
 
-def get_divisors(M):
-  X, Y = 2, floor(sqrt(M)) + 1
-  memo = [True] * Y
-  
-  for p in range(2, floor(sqrt(Y - 1)) + 1):
-    if not memo[p]:
-      continue
-
-    for q in range(p * 2, Y, p):
-      memo[q] = False
-      
-  return [x for x in range(X, Y) if memo[x]]
-
 # K = P^2 * Q
 def solve(N, M):
   result = M - N + 1
-  memo = [False] * (M - N + 1)
+  memo = set()
   
-  divisors = get_divisors(M)
-  
-  for X in divisors:
+  for X in range(2, floor(sqrt(M)) + 1):
     divisor = X ** 2
     offset = ceil(N / divisor) * divisor
     
     for K in range(offset, M + 1, divisor):
-      if memo[K - N]:
+      if K in memo:
         continue
         
-      memo[K - N] = True
+      memo.add(K)
       result -= 1
     
   return result
