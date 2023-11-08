516f2cf7a9aa910bef74cdcc12df6e7cec30b3ca
Jungheon Lee <cutehammond772@gmail.com>
2023-11-08 13:50:49 +0900

    [Gold I] Title: Street Checkers, Time: 19816 ms, Memory: 111560 KB, Score: 42 point -BaekjoonHub

--- 백준/Gold/23943. Street Checkers/Street Checkers.py
+++ 백준/Gold/23943. Street Checkers/Street Checkers.py
@@ -1,37 +1,58 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-def resolve(x):
-	# [even, odd]
-	result = [0, 0]
+def preprocess(N):
+	N = int(N ** 0.5) + 1
+	check = [True] * N
 
-	for i in range(1, int(x ** 0.5) + 1):
-		if not x % i:
-			p, q = i, x // i
+	check[0] = False
+	check[1] = False
 
-			result[p % 2] += 1
+	for i in range(2, int(N ** 0.5) + 1):
+		if not check[i]:
+			continue
 
-			if p != q:
-				result[q % 2] += 1
+		for n in range(i << 1, N, i):
+			check[n] = False
 
-	return result
+	return [x for x in range(N) if check[x]]
+
+def check(P, X):
+	for prime in P:
+		if X <= prime:
+			return 1
+
+		if not X % prime:
+			return 0
+
+	return 1
+
+# 2가 0개 : 짝수가 0개이므로 나머지 수는 "소수"여야 한다.
+# 2가 1개 : 짝수와 홀수의 개수는 무조건 같으므로 0 => 무조건 성립
+# 2가 2개 : 나머지 수는 무조건 소수 (또는 1)여야 한다.
+# 2가 3개 : 8만 성립한다.
+# 2가 4개 이상 : 불가능
+def resolve(P, x):
+	if not (x % (2 ** 3)):
+		return 1 if x == 8 else 0
+
+	if not (x % (2 ** 2)):
+		return check(P, x >> 2)
+
+	if not (x % (2 ** 1)):
+		return 1
+
+	return check(P, x)
 
 if __name__ == '__main__':
 	T = int(input())
-	memo = dict()
+	P = preprocess(10 ** 9)
 
 	for i in range(1, T + 1):
 		L, R = map(int, input().split())
 		result = 0
 
 		for t in range(L, R + 1):
-			if t in memo:
-				result += memo[t]
-				continue
-
-			even, odd = resolve(t)
-			memo[t] = 1 if abs(even - odd) <= 2 else 0
-
-			result += memo[t]
+			result += resolve(P, t)
 
 		print(f"Case #{i}: {result}")
