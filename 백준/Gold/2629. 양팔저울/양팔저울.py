d449a842a2fa8faf0f90fc2efe10df958663c037
Jungheon Lee <cutehammond772@gmail.com>
2023-11-12 22:46:16 +0900

    [Gold III] Title: 양팔저울, Time: 128 ms, Memory: 109604 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -3,33 +3,29 @@ input = lambda: sys.stdin.readline().rstrip()
 OFF = 15000
 
 def solve(N, M, A, B):
-  memo = [[False] * 30001 for _ in range(N + 1)]
-  memo[0][OFF] = True
+  memo = [False] * 30001
+  memo[OFF] = True
 
   for x in range(1, N + 1):
-    # 무게추를 사용하지 않는 경우
-    for i in range(30001):
-      memo[x][i] |= memo[x - 1][i]
-
     # 무게추를 구슬 쪽에 놓는 경우
     for i in range(A[x], 30001):
-      memo[x][i - A[x]] |= memo[x - 1][i]
+      memo[i - A[x]] |= memo[i]
 
     # 무게추를 추가하는 경우
-    for i in range(30001 - A[x]):
-      memo[x][i + A[x]] |= memo[x - 1][i]
+    for i in range(30000 - A[x], -1, -1):
+      memo[i + A[x]] |= memo[i]
 
   result = []
 
   for x in range(M):
     marble = B[x]
-    
+
     # 최대 15000g까지만 잴 수 있다.
     if marble > 15000:
       result.append('N')
       continue
 
-    result.append('Y' if memo[N][marble + OFF] else 'N')
+    result.append('Y' if memo[marble + OFF] else 'N')
 
   return result
 
