c193090d14d4fbba2b57d2846259aa7d59b754bc
Jungheon Lee <cutehammond772@gmail.com>
2023-11-12 21:54:12 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,24 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == '__main__':
+  N = int(input())
+  order = []
+
+  for _ in range(N):
+    x, y = map(int, input().split())
+    order.append((x, y))
+
+  order.sort()
+  seq = [o[1] for o in order]
+
+  # seq의 LIS의 길이가 곧 전깃줄의 최대 매칭이다.
+  L = len(seq)
+  memo = [1] * L
+
+  for x in range(L):
+    for y in range(x):
+      if seq[y] < seq[x]:
+        memo[x] = max(memo[x], 1 + memo[y])
+
+  print(L - max(memo))
