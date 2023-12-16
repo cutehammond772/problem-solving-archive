9613c3892b7cc7f9bf2d8aa1c9ed56f16d5de10c
Jungheon Lee <cutehammond772@gmail.com>
2023-12-16 23:59:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 392 ms, Memory: 136996 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve(R, G):
+  queue = deque([])
+
+  orders = [0] * (N + 1)
+  order = 1
+
+  # 시작 정점 설정
+  queue.append(R)
+  orders[R] = 1
+  order += 1
+
+  while queue:
+    node = queue.popleft()
+
+    for next in G[node]:
+      if orders[next]:
+        continue
+
+      orders[next] = order
+      order += 1
+
+      queue.append(next)
+
+  return orders[1:]
+
+if __name__ == "__main__":
+  N, M, R = map(int, input().split())
+  G = [[] for _ in range(N + 1)]
+
+  # 1. 그래프 구성
+  for _ in range(M):
+    u, v = map(int, input().split())
+
+    G[u].append(v)
+    G[v].append(u)
+
+  # 2. 인접 정점의 집합을 오름차순으로 정렬
+  for node in range(1, N + 1):
+    G[node].sort()
+
+  result = solve(R, G)
+  print(*result, sep='\n')
