4ca98930d1f1602e95e18b8e0365076bd9982e0a
Jungheon Lee <cutehammond772@gmail.com>
2024-01-26 00:05:29 +0900

    [Gold III] Title: 파티, Time: 696 ms, Memory: 118856 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,42 +1,44 @@
 import sys
 from heapq import heappush, heappop
+input = lambda: sys.stdin.readline().rstrip()
+INF = int(1e10)
 
-input = lambda: sys.stdin.readline().strip()
-NaN = 100000
+def dijkstra(N, G, S):
+	memo = [INF] * (N + 1)
+	
+	heap = [(0, S)]
+	memo[S] = 0
+	
+	while heap:
+		dist, node = heappop(heap)
+		
+		if memo[node] < dist:
+			continue
+		
+		for next, cost in G[node]:
+			next_dist = memo[node] + cost
+			
+			if memo[next] <= next_dist:
+				continue
+			
+			memo[next] = next_dist
+			heappush(heap, (next_dist, next))
+	
+	return memo
 
+def solve(N, X, G):
+	dist = [dijkstra(N, G, node) for node in range(1, N + 1)]
+	
+	return max(dist[node - 1][X] + dist[X - 1][node] for node in range(1, N + 1))
 
-def dijkstra(N, edges, P, Q):
-  heap = []
-  dist = [NaN] * N
-
-  dist[P] = 0
-  heappush(heap, (0, P))
-
-  while heap:
-    _, idx = heappop(heap)
-
-    for edge in edges[idx]:
-      next, cost = edge
-
-      if dist[next] > dist[idx] + cost:
-        dist[next] = dist[idx] + cost
-
-        if next != Q:
-          heappush(heap, (cost, next))
-
-  return dist[Q]
-
-
-if __name__ == '__main__':
-  N, M, X = map(int, input().split())
-  edges = [set() for _ in range(N)]
-
-  for _ in range(M):
-    P, Q, T = map(int, input().split())
-    edges[P - 1].add((Q - 1, T))
-
-  print(
-    max([
-      dijkstra(N, edges, t, X - 1) + dijkstra(N, edges, X - 1, t)
-      for t in range(N)
-    ]))
+if __name__ == "__main__":
+	N, M, X = map(int, input().split())
+	G = [[] for _ in range(N + 1)]
+	
+	for _ in range(M):
+		P, Q, C = map(int, input().split())
+		
+		G[P].append((Q, C))
+	
+	print(solve(N, X, G))
+	
