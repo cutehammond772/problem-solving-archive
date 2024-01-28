83e0263b165d0a01f95d006f971743d052b1ae4a
Jungheon Lee <cutehammond772@gmail.com>
2024-01-28 16:01:19 +0900

    [Gold IV] Title: LCS 2, Time: 652 ms, Memory: 308036 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,38 +1,35 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-def solve(P, Q):
-  memo = [[(0, "")] * len(Q) for _ in range(len(P))]
-  
-  for p in range(len(P)):
-    for q in range(len(Q)):
-      if P[p] == Q[q]:
-        if p > 0 and q > 0:
-          length, result = memo[p - 1][q - 1]
-          memo[p][q] = (length + 1, result + P[p])
-        else:
-          memo[p][q] = (1, P[p])
-      else:
-        candidates = [(0, "")]
-        
-        if p > 0:
-          candidates.append(memo[p - 1][q])
-          
-        if q > 0:
-          candidates.append(memo[p][q - 1])
-          
-        if p > 0 and q > 0:
-          candidates.append(memo[p - 1][q - 1])
+def lcs(P, Q):
+	LP, LQ = len(P), len(Q)
+	
+	# (size, (current_index), (previous index))
+	memo = [[(0, (p, q), (p, q)) for q in range(LQ + 1)] for p in range(LP + 1)]
+	
+	for p in range(1, LP + 1):
+		for q in range(1, LQ + 1):
+			memo[p][q] = max(memo[p - 1][q], memo[p][q - 1], memo[p - 1][q - 1])
+			
+			if P[p - 1] == Q[q - 1]:
+				prev_size, prev_index, _ = memo[p - 1][q - 1]
+				memo[p][q] = max(memo[p][q], (prev_size + 1, (p, q), prev_index))
+	
+	size, curr_idx, prev_idx = memo[LP][LQ]
+	sequence = []
+	
+	for _ in range(size):
+		sequence.append(P[curr_idx[0] - 1])
+		curr_idx, prev_idx = memo[prev_idx[0]][prev_idx[1]][1:]
+	
+	return "".join(sequence[::-1])
 
-        memo[p][q] = max(candidates, key = lambda x: x[0])
-
-  return memo[len(P) - 1][len(Q) - 1]
-  
-if __name__ == '__main__':
-  P, Q = input(), input()
-  length, result = solve(P, Q)
-  
-  print(length)
-  
-  if len(result) > 0:
-    print(result)
+if __name__ == "__main__":
+	P, Q = input(), input()
+	result = lcs(P, Q)
+	
+	print(len(result))
+	
+	if result:
+		print(result)
+	
