e71eccbf46aab184a011051ce7f986435c184401
Jungheon Lee <cutehammond772@gmail.com>
2024-01-30 14:33:18 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,34 +1,18 @@
 def solution(n, lost, reserve):
-    answer = 0
-    status = [1] * (n + 2)
+    answer = n
     
-    # 1 ~ n번째 학생만 고려함.
-    status[0] = status[n + 1] = 0
+    _reserve = set(reserve) - set(lost)
+    _lost = set(lost) - set(reserve)
     
-    for lost_student in lost:
-        status[lost_student] -= 1
-    
-    for reserve_student in reserve:
-        status[reserve_student] += 1
-    
-    for student in range(1, n + 1):
-        answer += 1
+    for student in _lost:
+        if student - 1 in _reserve:
+            _reserve.remove(student - 1)
         
-        if status[student] != 0:
-            continue
+        elif student + 1 in _reserve:
+            _reserve.remove(student + 1)
         
-        # 왼쪽 학생부터 먼저 판단한다.
-        if status[student - 1] > 1:
-            status[student - 1] -= 1
-            status[student] = 1
-        
-        elif status[student + 1] > 1:
-            status[student + 1] -= 1
-            status[student] = 1
-        
-        # 체육복을 빌릴 수 없는 경우를 나타낸다.
         else:
             answer -= 1
-    
+        
     return answer
     
