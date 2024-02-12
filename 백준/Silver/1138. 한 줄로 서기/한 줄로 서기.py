33df18547b853d1513a2578312d98d4fa015c5b3
Jungheon Lee <cutehammond772@gmail.com>
2024-02-12 20:33:26 +0900

    [Silver II] Title: 한 줄로 서기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,23 +1,24 @@
 import sys
-from itertools import permutations
 input = lambda: sys.stdin.readline().rstrip()
 
+# 키가 작은 사람부터, 왼쪽에 (키가 더 큰 사람들의) 자리를 만들어 주면 된다.
 def solve(N, A):
-	for sequence in permutations(range(1, N + 1)):
-		check = [0] * N
-		
-		for x in range(N):
-			for y in range(x):
-				check[sequence[x] - 1] += sequence[y] > sequence[x]
-		
-		if tuple(A) == tuple(check):
-			return sequence
+	result = [0] * N
 	
-	return None
+	for x in range(N):
+		count = 0
+		
+		for k in range(N):
+			if count == A[x] and (not result[k]):
+				result[k] = 1 + x
+				break
+				
+			count += (not result[k]) or (result[k] > 1 + x)
+		
+	return result
 
 if __name__ == "__main__":
 	N = int(input())
 	A = [*map(int, input().split())]
 	
 	print(*solve(N, A))
-		
