22fe17105ca22cb9bb02eb30679023f7466fe4af
Jungheon Lee <cutehammond772@gmail.com>
2024-04-04 14:04:08 +0900

    [Diamond IV] Title: XOR MST, Time: 556 ms, Memory: 86500 KB -BaekjoonHub

--- 백준/Diamond/16901. XOR MST/XOR MST.cc
+++ 백준/Diamond/16901. XOR MST/XOR MST.cc
@@ -2,72 +2,65 @@
 #pragma GCC optimize("unroll-loops")
 
 #include <bits/stdc++.h>
+
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define INF (1<<30)
 
 using namespace std;
 using ll = long long;
 
 struct Trie {
-    Trie *zero = nullptr, *one = nullptr;
+    Trie *node[2] = { nullptr, nullptr };
     int index = 0;
 
-    Trie(int index): index(index) {}
+    explicit Trie(int index): index(index) {}
 };
 
 int N, A;
-
 Trie* root = new Trie(30);
 
 ll minimum(Trie* left, Trie* right) {
-    ll result = 1 << 30;
+    ll result = INF;
     bool diff = true;
 
-    // 두 자리의 부호가 같은 경우
-    if (left->zero && right->zero) {
-        diff = false;
-        result = min(result, minimum(left->zero, right->zero));
+    // 1. 두 자리의 부호가 같은 경우
+    for (int bit = 0; bit <= 1; bit++) {
+        if (left->node[bit] && right->node[bit]) {
+            diff = false;
+            result = min(result, minimum(left->node[bit], right->node[bit]));
+        }
     }
 
-    if (left->one && right->one) {
-        diff = false;
-        result = min(result, minimum(left->one, right->one));
+    if (!diff) return result;
+
+    // 2. 두 자리의 부호가 다른 경우
+    for (int bit = 0; bit <= 1; bit++) {
+        if (left->node[bit] && right->node[1 - bit]) {
+            result = min(result, (1 << ((left->index) - 1)) | minimum(left->node[bit], right->node[1 - bit]));
+        }
     }
 
-    if (!diff)
-        return result;
-
-    if (left->zero && right->one) {
-        result = min(result, (1 << ((left->index) - 1)) | minimum(left->zero, right->one));
-    }
-
-    if (left->one && right->zero) {
-        result = min(result, (1 << ((left->index) - 1)) | minimum(left->one, right->zero));
-    }
-
-    if (result == 1 << 30)
-        return 0;
-
-    return result;
+    return (result != INF) ? result : 0;
 }
 
 ll traverse(Trie *node, ll common) {
-    if (!node->zero && !node->one)
+    if (!node->node[0] && !node->node[1])
         return 0;
 
     int addition = 1 << ((node->index) - 1);
 
-    if (!node->zero)
-        return traverse(node->one, common | addition);
+    if (!node->node[0])
+        return traverse(node->node[1], common | addition);
 
-    if (!node->one)
-        return traverse(node->zero, common);
+    if (!node->node[1])
+        return traverse(node->node[0], common);
 
-    // 1. 두 컴포넌트에 대해 계산
-    ll zero = traverse(node->zero, common);
-    ll one = traverse(node->one, common | addition);
+    // 1. 두 컴포넌트에 대해 MST 비용 구하기
+    ll zero = traverse(node->node[0], common);
+    ll one = traverse(node->node[1], common | addition);
 
     // 2. 두 컴포넌트 간 가장 작은 간선의 비용 구하기
-    ll cost = addition | minimum(node->zero, node->one);
+    ll cost = addition | minimum(node->node[0], node->node[1]);
 
     return (zero + one) + cost;
 }
@@ -83,16 +76,9 @@ int main() {
 
         for (int x = 29; x >= 0; x--) {
             int bit = (A & (1 << x)) >> x;
+            if (!node->node[bit]) node->node[bit] = new Trie(x);
 
-            if (bit == 0) {
-                if (!node->zero) { node->zero = new Trie(x); }
-                node = node->zero;
-            }
-
-            if (bit == 1) {
-                if (!(node->one)) node->one = new Trie(x);
-                node = node->one;
-            }
+            node = node->node[bit];
         }
     }
 
